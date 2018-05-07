f87988067b51a18e7f1ae1faf22a09b43723b89a
vibhutikumar11 <vibhutikumar11@gmail.com>
2018-05-07 16:18:39 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+
+interface DataResponse {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent implements OnInit {
+  title = 'app';
+  myData : any;
+
+
+  constructor(private http: HttpClient) {}
+
+  showAllPost(){
+        this.http.get<DataResponse>('http://jsonplaceholder.typicode.com/posts').subscribe(data => {
+          this.myData = data;
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log('Client-side error occured.');
+          } else {
+            console.log('Server-side error occured.');
+          }
+        }
+      );
+  }
+
+  ngOnInit() { 
+   
+  }
+
+
+
+}
